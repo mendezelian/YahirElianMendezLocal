522c0e948553797f1e0e042674b9684d6825aab0
McMac31 <miguel317.angelps@gmail.com>
2025-02-25 11:26:32 +0100

    He añadido nuevos atributos para saber a que asignatura pertenece el examen y el estado en el que se encuentra

--- app.java
+++ app.java
@@ -2,11 +2,15 @@ public class Examen{
 	private String Nombre;
 	private double Nota;
 	private String Tutor;
+	private String []Asignatura;
+	private boolean Aprobado;
 	
-	public Examen(String Nota, double Nota, String Tutor){
+	public Examen(String Nota, double Nota, String Tutor, String[]Asignatura, boolean Aprobado){
 		this.Nombre = Nombre;
 		this.Nota = 10;
 		this.Tutor = Tutor;
-	}
+		this.Asignatura= 'Matematicas';
+		this.Aprobado= true;
+}
 
 }
