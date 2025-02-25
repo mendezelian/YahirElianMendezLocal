5da378fe604a65026bf8847d6af5cbd9fd790571
YAHIR MENDEZ <ikyemendez24@lhusurbil.eus>
2025-02-25 09:43:44 +0100

    Commit 2

--- app.java
+++ app.java
@@ -1,10 +1,12 @@
 public class Examen{
 	private String Nombre;
 	private double Nota;
+	private String Tutor;
 	
-	public Examen(String Nota, double Nota){
+	public Examen(String Nota, double Nota, String Tutor){
 		this.Nombre = Nombre;
 		this.Nota = 10;
+		this.Tutor = Tutor;
 	}
 
 }
